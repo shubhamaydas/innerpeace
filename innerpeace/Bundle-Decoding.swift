490d1cc129613fa588badd935e76760990e0e37f
Shubhamay <shubhamaydas1@gmail.com>
2022-07-02 14:46:22 +0200

    inital commit

---
+++ innerpeace/Bundle-Decoding.swift
@@ -0,0 +1,14 @@
+//
+//  Bundle-Decoding.swift
+//  innerpeace
+//
+//  Created by Shubhamay Das on 02.07.22.
+//
+
+import Foundation
+
+extension Bundle {
+    func decode<T: Decodable>(_ type: T.Type, from: file: String) {
+        
+    }
+}
--- innerpeace/ViewController.swift
+++ innerpeace/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  innerpeace
-//
+// 
 //  Created by Shubhamay Das on 02.07.22.
 //
 
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var quoteImageView: UIImageView!
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
